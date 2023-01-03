55df46c952f5bf2315608c52ba28fdb556661bb9
brndnwd <bdwood@protonmail.com>
2023-01-02 19:56:00 -0500

    Pascals config changes

--- keyboards/handwired/tractyl_manuform/5x6_right/elite_c/config.h
+++ keyboards/handwired/tractyl_manuform/5x6_right/elite_c/config.h
@@ -18,13 +18,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// wiring of each half
-#define MATRIX_COL_PINS \
-    //{ D4, D7, E6, B4, B5, B7 }
-    { D4, C6, D7, E6, B4, B5 }
-#define MATRIX_ROW_PINS \
-    //{ F7, F6, F5, F4, F1, F0 }
-    { B7, D5, C7, F1, F0, B6 }
+// wiring of left half
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+
+// wiring of right half
+#define MATRIX_COL_PINS_RIGHT { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS_RIGHT { B7, D5, C7, F1, F0, B6 }
 
 #define DIODE_DIRECTION COL2ROW
 
@@ -36,11 +36,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { 10, 10 }
 #define RGBLIGHT_LIMIT_VAL 80
 
-#define AUDIO_PIN       F6
+#define AUDIO_PIN       C6
 
 /* serial.c configuration for split keyboard */
-#define SOFT_SERIAL_PIN D2
+#define SOFT_SERIAL_PIN D0
 #define EE_HANDS
+#define MASTER_RIGHT
 
 #define ENCODERS_PAD_A \
     { D5 }
@@ -48,4 +49,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     { C7 }
 
 /* PMW33XX Settings */
-#define PMW33XX_CS_PIN B6
+#define PMW33XX_CS_PIN F7
