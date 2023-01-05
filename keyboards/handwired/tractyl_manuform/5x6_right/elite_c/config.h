5e7f4377fc93815f599c0c627905dc1118306169
brndnwd <bdwood@protonmail.com>
2023-01-04 22:41:10 -0500

    trying split_usb_detect config

--- keyboards/handwired/tractyl_manuform/5x6_right/elite_c/config.h
+++ keyboards/handwired/tractyl_manuform/5x6_right/elite_c/config.h
@@ -40,6 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* serial.c configuration for split keyboard */
 #define SOFT_SERIAL_PIN D0
+#define SPLIT_USB_DETECT
 //#define EE_HANDS
 #define MASTER_RIGHT
 
